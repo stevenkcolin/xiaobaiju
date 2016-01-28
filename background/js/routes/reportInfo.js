0947b85365591adb14cc38e23326c21724603896
hepf <hepf33@hotmail.com>
2016-01-28 13:54:48 +0800

    增加/reportInfo/massCreate接口

--- background/js/routes/reportInfo.js
+++ background/js/routes/reportInfo.js
@@ -1,15 +1,12 @@
 /**
- * Created by linchen on 1/24/16.
+ * Created by Pengfei on 1/24/16.
  */
-
-
-
 var express = require("express");
-var _ = require('underscore');
 
 var models = require("../models/sqlModel");
 var successHandler = require("../common/successHandler");
 var ActionType = models.ActionType;
+var transactionHandler = require("../common/transactionHandler");
 
 var router = express.Router();
 
@@ -19,7 +16,7 @@ router.all("*", function(req, res, next) {
     next();
 });
 
-// create a ActionType
+// create a ReportInfo
 router.post("/create", function(req, res) {
     var reportInfo = req.body;
     var reportModel = req.models.reportInfo;
@@ -28,5 +25,25 @@ router.post("/create", function(req, res) {
         successHandler.handle(null, res);
     });
 });
+
+// create multiple ReportInfo
+router.post("/massCreate", [transactionHandler.begin,
+        function(req, res, next) {
+            var reportInfoList = req.body;
+            if (reportInfoList.length === 0) next();
+            var reportModel = req.models.reportInfo;
+            var count = 0;
+            for (var i in reportInfoList) {
+                reportInfoList[i].actionTime = new Date(reportInfoList[i].actionTime);
+                reportModel.create(reportInfoList[i], function (err) {
+                    if (err) req.models.error = err;
+                    count++;
+                    if (count === reportInfoList.length) next();
+                });
+            }
+        }, transactionHandler.finalize,
+    function(req, res) {
+        successHandler.handle(null, res)
+    }]);
 
 module.exports = router;
