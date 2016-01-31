2ba1a1a14abb2ed7acccaf6c524e80d6a9ac04ea
kursk2339 <dream1945@163.com>
2016-01-31 10:04:12 +0800

    nathan

---
+++ background/js/routes/admin.js
@@ -0,0 +1,145 @@
+﻿/**
+ * Created by Pengfei on 2015/11/24.
+ * router for /admin url
+ */
+
+var express = require("express");
+var _ = require('underscore');
+
+var models = require("../models/model");
+var md5encrypt = require("../common/md5encrypt");
+var successHandler = require("../common/successHandler");
+var Admin = models.Admin;
+
+var router = express.Router();
+
+// set a flag
+router.all("*", function(req, res, next) {
+	req.isRouted = true;
+	next();
+});
+
+
+// create a admin
+router.post("/create", function(req, res, next) {
+	var admin = new Admin();
+	//if (req.body.password) req.body.password = md5encrypt.encrypt(req.body.password);
+	_.extend(admin, req.body);
+    admin.createDate = new Date();
+    admin.save(function(err, doc){
+		if (err) throw err;
+		successHandler.handle(doc, res);
+	});
+});
+
+// delete admin
+router.post("/delete", function(req, res, next) {
+	var adminIdsArray = req.body.adminIds.split(",");
+
+    Admin.remove({"_id" : {"$in" : adminIdsArray } },function(err, doc) {
+        if (err) throw err;
+        successHandler.handle(doc, res);
+    })
+
+});
+
+// update admin
+router.post("/update", function(req, res, next) {
+    var adminId = req.body.adminId;
+    var param = req.body;
+    Admin.update({"_id" : adminId}, {$set:param},function(err, doc) {
+        if (err) throw err;
+        successHandler.handle(doc, res);
+    })
+});
+
+// find admin by id
+router.post("/searchById", function(req, res) {
+    var adminId = req.body.adminId;
+    var searchCondition = {"_id": adminId};
+
+    Admin.find(searchCondition,function(err,doc){
+        if (err) throw err;
+        successHandler.handle(doc, res);
+    });
+});
+
+// find admin by search condition
+router.post("/searchByCondition", function(req, res, next) {
+
+    if (typeof req.body.name === "undefined") {
+        successHandler.handle(null, res);
+        return;
+    }
+
+    var name = req.body.name;
+    var loginAccount = req.body.loginAccount;
+    var searchCondition = {"name": new RegExp(name, 'i'),"loginAccount": loginAccount};
+    if (name === '') {
+        delete searchCondition.name;
+    }
+    if (loginAccount === '') {
+        delete searchCondition.loginAccount;
+    }
+
+    var page=req.body.page;
+    var rows=req.body.rows;
+    var startLine = (page-1)*rows;
+    var dbSearch = Admin.find(searchCondition).limit(+rows).skip(startLine);
+
+    dbSearch.exec(function(err,rs){
+        if(err){
+            throw err;
+        }else{
+            //计算数据总数
+            Admin.find(searchCondition, function(err,result){
+                res.json({rows:rs,total:result.length});
+            });
+
+        }
+    });
+
+});
+
+// login by phone and password
+router.post("/login", function(req, res, next) {
+    var phone = req.body.phone;
+    var password = req.body.password;
+    var encoded = req.body.encoded;
+    if (encoded !== true) {
+        password = md5encrypt.encrypt(password);
+    }
+    User.find({phone: phone, password: password}, function(err, doc) {
+        if (err) throw err;
+        successHandler.handle(doc, res);
+    });
+});
+
+// login by 3rd
+router.post("/loginByOther", function(req, res, next) {
+    var loginFrom = req.body.loginFrom;
+    var loginAccount = req.body.loginAccount;
+    User.find({loginFrom: loginFrom, loginAccount: loginAccount}, function(err, doc) {
+        if (err) throw err;
+        successHandler.handle(doc, res);
+    });
+});
+
+router.get('/', function(req, res, next) {
+	res.send('respond with a resource');
+});
+
+router.get('/main', function(req, res) {
+    res.render('adminMain', { title: 'Hello, World!' });
+});
+
+router.get('/adminMgmt', function(req, res) {
+    res.render('adminMgmt', {});
+});
+
+// 定义 about 页面的路由
+router.get('/about', function(req, res) {
+	res.send('About birds');
+});
+
+module.exports = router;
