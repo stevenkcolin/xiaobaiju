d79e33a5de39696f23580a4b7a7d5c655dba4c83
kursk2339 <dream1945@163.com>
2016-02-01 17:20:47 +0800

    nathan test

--- background/js/routes/admin/index.js
+++ background/js/routes/admin/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/index', function(req, res, next) {
-  res.render('index', { username: req.session.user[0].username });
+  res.render('index', { username: req.session.user[0].name });
 });
 
 router.get('/login', function(req, res) {
